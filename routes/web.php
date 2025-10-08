d30780e516f1e941a45bd88f582cd419f7e65cda
VenomError <venoms00001@gmail.com>
2025-10-08 22:18:10 +0800

    change route

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 // Authentication
-Route::controller(AuthController::class)->middleware('guest')->group(function () {
+Route::middleware('guest')->group(function () {
 
-    Route::get('/login', 'login')->name('login');
-    Route::post('/login', 'loginProcess')->name('loginProcess');
-    Route::get('/register', 'register')->name('register');
-    Route::post('/register', 'registerProcess')->name('registerProcess');
+    Route::get('/login', [AuthController::class,'login'])->name('login');
+    Route::post('/login', [AuthController::class,'loginProcess'])->name('loginProcess');
+    Route::get('/register', [AuthController::class,'register'])->name('register');
+    Route::post('/register', [AuthController::class,'registerProcess'])->name('registerProcess');
 
 });
 Route::get('/logout', [AuthController::class, 'logout'])->middleware('auth')->name('logout');
